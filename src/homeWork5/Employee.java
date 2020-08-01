ca2c1db58bf7cd9275c588bea6e246f7ab4f5e79
gavrish <gavrish@lanit.ru>
2020-08-01 21:52:06 +0300

    Решение ДЗ 5

---
+++ src/homeWork5/Employee.java
@@ -0,0 +1,28 @@
+package homeWork5;
+
+public class Employee {
+    private String name;
+    private String position;
+    private String email;
+    private String phone;
+    private double salary;
+    private int age;
+
+    public Employee(String name, String position, String email, String phone, double salary, int age) {
+        this.name = name;
+        this.position = position;
+        this.email = email;
+        this.phone = phone;
+        this.salary = salary;
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void printInfo() {
+        System.out.printf("Информация по сотруднику:\n" +
+            "ФИО: %s\nДолжность: %s\nEmail: %s, телефон: %s\nЗарплата: %.2f\nВозраст: %d\n", name, position, email, phone, salary, age);
+    }
+}
