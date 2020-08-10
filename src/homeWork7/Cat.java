877a71bc7a9070d5c0298f5dcd943c1a923f1ec5
gavrish <gavrish@lanit.ru>
2020-08-10 12:00:11 +0300

    Решение ДЗ 7

---
+++ src/homeWork7/Cat.java
@@ -0,0 +1,37 @@
+package homeWork7;
+
+public class Cat {
+    private String name;
+    private int foodAbility;
+    private boolean satiety;
+
+    public Cat(String name, int foodAbility) {
+        this.name = name;
+        this.foodAbility = foodAbility;
+        satiety = false;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void eat(Plate plate) {
+        if (plate.isFoodEnough(foodAbility)) {
+            plate.decreaseFood(foodAbility);
+            satiety = true;
+            System.out.printf("Cat %s is eating\n", name);
+        } else {
+            System.out.printf("Cat %s is not eating, because he wants %d units of food " +
+                "and plate has only %d units.\n", name, foodAbility, plate.getFood());
+        }
+    }
+
+    public boolean isSatiety() {
+        return satiety;
+    }
+
+    @Override
+    public String toString() {
+        return "Cat " + name + " is" + (satiety ? "" : " not") + " full";
+    }
+}
---
+++ src/homeWork7/calculator/Main.java
@@ -0,0 +1,10 @@
+package homeWork7.calculator;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        calculator.setVisible(true);
+        calculator.pack();
+    }
+}
