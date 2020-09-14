09e6e85d01549f3c8bd73746908695c83fad58cc
gavrish <gavrish@lanit.ru>
2020-09-14 14:42:57 +0300

    ДЗ 8

--- src/level2/homeWork8/server/ClientHandler.java
+++ src/level2/homeWork8/server/ClientHandler.java
@@ -4,6 +4,7 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
+import java.net.SocketTimeoutException;
 
 public class ClientHandler {
     private Server server;
@@ -23,7 +24,7 @@ public class ClientHandler {
 
             new Thread(() -> {
                 try {
-//                    socket.setSoTimeout(5000);
+                    socket.setSoTimeout(5000);
                     //цикл аутентификации
                     while (true) {
                         String str = in.readUTF();
@@ -43,6 +44,8 @@ public class ClientHandler {
                                     sendMsg("/authok " + nickname);
                                     server.subscribe(this);
                                     System.out.println("Клиент " + nickname + " подключился");
+                                    sendMsg("Пользователь " + nickname + " присоединился");
+                                    socket.setSoTimeout(0);
                                     break;
                                 } else {
                                     sendMsg("С данной учетной записью уже зашли");
@@ -92,6 +95,8 @@ public class ClientHandler {
                     }
 
                     //SocketTimeoutException
+                } catch (SocketTimeoutException e) {
+                    System.out.println("Клиент не аторизовался в течении 5 секунд");
                 } catch (IOException e) {
                     e.printStackTrace();
                 } finally {
