5306ed63068c2a7512245c6b91299423e5da4812
gavrish <gavrish@lanit.ru>
2020-10-01 12:28:25 +0300

    ДЗ4 ABC

--- src/level3/homeWork4/Main.java
+++ src/level3/homeWork4/Main.java
@@ -1,4 +1,60 @@
 package level3.homeWork4;
 
 public class Main {
+    static Object mon = new Object();
+    static volatile int currentNum = 1;
+    static final int num = 5;
+
+    public static void main(String[] args) {
+        new Thread(() -> {
+            try {
+                for (int i = 0; i < num; i++) {
+                    synchronized (mon) {
+                        while (currentNum != 1) {
+                            mon.wait();
+                        }
+                        System.out.print("A ");
+                        currentNum = 2;
+                        mon.notifyAll();
+                    }
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
+
+        new Thread(() -> {
+            try {
+                for (int i = 0; i < num; i++) {
+                    synchronized (mon) {
+                        while (currentNum != 2) {
+                            mon.wait();
+                        }
+                        System.out.print("B ");
+                        currentNum = 3;
+                        mon.notifyAll();
+                    }
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
+
+        new Thread(() -> {
+            try {
+                for (int i = 0; i < num; i++) {
+                    synchronized (mon) {
+                        while (currentNum != 3) {
+                            mon.wait();
+                        }
+                        System.out.println("C ");
+                        currentNum = 1;
+                        mon.notifyAll();
+                    }
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
+    }
 }
